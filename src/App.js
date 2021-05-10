57eb34ccd82122150a4faa215e42e5432d5da9c7
Tayrhon <tayrhonstream@gmail.com>
2021-05-10 14:36:28 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/component/Navbar/NavbarElements.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Nav = style.nav`
+  background: transparent;
+  height: 80p;
+`
